ea50f56894f55cc6590f3a57839fde4e3e1f65cb
Achsan <achsanh@gmail.com>
2024-08-18 03:23:51 -0400

    🧑‍💻 feat(setup): add graceful shutdown

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/aikuci/go-subdivisions-id/internal/config"
 )
@@ -14,14 +18,26 @@ func main() {
 	db := config.NewDatabase(viperConfig, zapLog)
 	validate := config.NewValidator(viperConfig)
 
-	// Must be called within same function, check the [docs](https://go.dev/tour/flowcontrol/12)
-	// Ref: https://www.reddit.com/r/golang/comments/bkqxgm/comment/emivhxl/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 	file, err := os.OpenFile("storage/log/fiber.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
 	defer file.Close()
 	app := config.NewFiber(viperConfig, &config.AppOptions{LogWriter: file})
+
+	// Ref: https://github.com/gofiber/fiber/issues/899#issuecomment-1429739373
+	var serverShutdown sync.WaitGroup
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Println("Gracefully shutting down...")
+		serverShutdown.Add(1)
+		defer serverShutdown.Done()
+		if err := app.ShutdownWithTimeout(60 * time.Second); err != nil {
+			log.Fatalf("failed to shut down server: %v", err)
+		}
+	}()
 
 	config.Bootstrap(&config.BootstrapConfig{
 		DB:       db,
@@ -32,8 +48,12 @@ func main() {
 	})
 
 	webPort := viperConfig.GetInt("web.port")
-	err = app.Listen(fmt.Sprintf(":%d", webPort))
-	if err != nil {
+	log.Printf("Starting REST, listening at %d\n", webPort)
+	if err := app.Listen(fmt.Sprintf(":%d", webPort)); err != nil {
 		log.Fatalf("failed to start server: %v", err)
 	}
+
+	serverShutdown.Wait()
+	fmt.Println("Running cleanup tasks...")
+	// Cleanup tasks
 }
