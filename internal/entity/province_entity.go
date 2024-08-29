0dcc7d65bb83ceec4fb115d2f4494376c6ed2e72
Achsan <achsanh@gmail.com>
2024-08-29 02:21:53 -0400

    🗃️ feat(village): register entity relationships

--- internal/entity/province_entity.go
+++ internal/entity/province_entity.go
@@ -9,6 +9,7 @@ type Province struct {
 	PostalCodes pq.Int64Array `gorm:"column:postal_codes;type:int4[]"`
 	Cities      []City        `gorm:"foreignKey:id_province"`
 	Districts   []District    `gorm:"foreignKey:id_province"`
+	Villages    []Village     `gorm:"foreignKey:id_province"`
 }
 
 func (p *Province) TableName() string {
