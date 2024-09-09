228bfc5bab9b8ee108cbdf0167baedd01b4f129a
Achsan <achsanh@gmail.com>
2024-09-09 07:17:26 -0400

    🌱 feat: add migration to initialize tables for testing environment

--- test/init.go
+++ test/init.go
@@ -1,9 +1,11 @@
 package test
 
 import (
+	"log"
 	"os"
 
 	"github.com/aikuci/go-subdivisions-id/internal/config"
+	"github.com/aikuci/go-subdivisions-id/internal/entity"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
@@ -17,18 +19,27 @@ var db *gorm.DB
 
 var viperConfig *viper.Viper
 
-var log *zap.Logger
+var zapLog *zap.Logger
 
 func init() {
 	viperConfig = config.NewViper()
-	log = config.NewZapLog(viperConfig)
+	zapLog = config.NewZapLog(viperConfig)
 	app = config.NewFiber(viperConfig, &config.AppOptions{LogWriter: os.Stdout})
 	db = config.NewDatabase(viperConfig)
 
 	config.Bootstrap(&config.BootstrapConfig{
 		App:    app,
 		Config: viperConfig,
-		Log:    log,
+		Log:    zapLog,
 		DB:     db,
 	})
+
+	initTables()
+}
+
+func initTables() {
+	err := db.AutoMigrate(&entity.Province{}, &entity.City{}, &entity.District{}, &entity.Village{})
+	if err != nil {
+		log.Fatalf("failed to initialize tables: %v", err)
+	}
 }
